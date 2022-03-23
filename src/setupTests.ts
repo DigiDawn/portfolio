f356e9c6e018d5c253a10af0a88278822afc622c
DigiDawn <rorylang781@gmail.com>
2022-03-23 13:30:04 -0400

    Test portfolio with jest

---
+++ src/setupTests.ts
@@ -0,0 +1,20 @@
+import '@testing-library/jest-dom';
+import { jest } from '@jest/globals';
+
+Object.defineProperty(Element.prototype, "scrollIntoView", {
+  value: jest.fn(),
+  writable: true,
+});
+
+window.matchMedia = jest.fn((query) => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: jest.fn(),
+  removeListener: jest.fn(),
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  dispatchEvent: jest.fn(),
+})) as unknown as typeof window.matchMedia;
+
+window.alert = jest.fn();
